c86a85aac708f852c24dd9debf0bf774719a5732
GJ Schouten <gj.schouten@lukashian.org>
2026-09-17 20:22:15 +0200

    Release 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.lukashian.calendarwidget"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
